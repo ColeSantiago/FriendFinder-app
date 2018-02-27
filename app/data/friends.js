acbc9a3925b1f24be598f6ddcab6c34e0ee55a48
Cole Santiago <colesantiago@Coles-MBP.fios-router.home>
2018-02-26 22:07:40 -0500

    survey answers are being saved on the server

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-
-	$('.submit-btn').on('click', function(event) {
+$('.submit-btn').on('click', function(event) {
 		event.preventDefault();
 		
-		let newFriends = {
+		let newFriend = {
 			name: $('#name').val().trim(),
 			photo: $('#photo').val().trim(),
 			scores: [$('#answer-1').val(), $('#answer-2').val(), $('#answer-3').val(), $('#answer-4').val(), $('#answer-5').val(),
 					 $('#answer-6').val(), $('#answer-7').val(), $('#answer-8').val(), $('#answer-9').val(), $('#answer-10').val()]
 		}
-		console.log(newFriends);
+		console.log(newFriend);
 	});
-});
+
+	$.post('/api/new', newFriend)
+	.then(function(data) {
+		console.log(data)
+	});
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const friends = [];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname, 'app/public/home.html'));
+});
+
+app.get('/survey', function(request, response) {
+	response.sendFile(path.join(__dirname, 'app/public/survey.html'));
+});
+
+app.get('/friends', function(request, response) {
+	response.json(friends);
+})
+
+app.post('/api/new', function(request, response) {
+	const newfriend = request.body;
+	console.log(newfriend);
+	friends.push(newfriend);
+	response.json(newfriend);
+});
+
+app.listen(PORT, function() {
+	console.log('App listening on PORT ' + PORT);
+});
+ 
